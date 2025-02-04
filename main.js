5b22dc1810fb606f6f29f7f13ece6074a3949295
JavierUnexca <lbpf2021jesr@gmail.com>
2025-02-04 19:37:21 -0400

    formulario, imagenes y arreglos

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
   mostrarFechas();
   initArticulos(); // Asegúrate de llamar a initArticulos aquí
   getBrowserInfo(); // Llamada a getBrowserInfo para asegurar que se ejecuta al cargar la página
+  initFormularios();
 });
 
 function mostrarFechas() {
@@ -52,4 +53,56 @@ function getBrowserInfo() {
   }
 
   document.getElementById("browser-info").textContent = `Navegador: ${browserName}, Versión: ${fullVersion}`;
+}
+
+// FORMULARIOS
+function initFormularios() {
+  const inputs = document.querySelectorAll('input[type="text"], textarea, input[type="email"], input[type="number"]');
+  const form = document.getElementById('personal-info-form');
+
+  inputs.forEach(input => {
+    input.addEventListener('focus', function () {
+      this.style.backgroundColor = '#f880de'; // Cambia el color de fondo al enfocar
+    });
+
+    input.addEventListener('blur', function () {
+      this.style.backgroundColor = ''; // Restaura el color de fondo original
+      this.value = this.value.toUpperCase(); // Convierte el texto a mayúsculas
+    });
+  });
+
+  document.getElementById("datos").onclick = function (event) {
+    event.preventDefault(); // Evita el envío del formulario por defecto
+    let valid = true;
+    inputs.forEach(input => {
+      if (!input.value) {
+        valid = false;
+        input.style.borderColor = 'red'; // Marca el campo vacío en rojo
+      } else {
+        input.style.borderColor = ''; // Restaura el color del borde
+      }
+    });
+
+    if (valid) {
+      alert('Nombre: ' + form.elements[0].value + '\n'
+         + 'Correo: ' + form.elements[1].value + '\n' 
+         + 'Edad: ' + form.elements[2].value + '\n');  
+      document.getElementById("login").style.display = "none";
+      document.getElementById("ocultar").style.display = "block";
+    } else {
+      alert('Por favor, complete todos los campos obligatorios.');
+    }
+  }
+}
+
+// Muestra el formulario de login
+function mostrarLogin() {
+  document.getElementById("login").style.display = "block";
+  document.getElementById("ocultar").style.display = "none";
+}
+
+// Muestra la página de Caos Melódico
+function mostrarOcultar() {
+  document.getElementById("login").style.display = "none";
+  document.getElementById("ocultar").style.display = "block";
 }
