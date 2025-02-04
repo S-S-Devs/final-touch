b8778f984ae8cab0d4c86e140e7094a87e857dd8
Monster User <monsteruser@gmail.com>
2025-02-04 18:51:48 -0400

    Footer

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
   mostrarFechas();
   initArticulos(); // Asegúrate de llamar a initArticulos aquí
+  getBrowserInfo(); // Llamada a getBrowserInfo para asegurar que se ejecuta al cargar la página
 });
 
 function mostrarFechas() {
@@ -24,4 +25,31 @@ function initArticulos() {
       botonVerMas.style.display = 'none';
     });
   }
+}
+
+function getBrowserInfo() {
+  const userAgent = navigator.userAgent;
+  let browserName, fullVersion;
+
+  if (userAgent.indexOf("Chrome") > -1 && userAgent.indexOf("Edg/") === -1) {
+      browserName = "Chrome";
+      fullVersion = userAgent.split("Chrome/")[1].split(" ")[0];
+  } else if (userAgent.indexOf("Edg/") > -1) {
+      browserName = "Microsoft Edge";
+      fullVersion = userAgent.split("Edg/")[1].split(" ")[0];
+  } else if (userAgent.indexOf("Firefox") > -1) {
+      browserName = "Firefox";
+      fullVersion = userAgent.split("Firefox/")[1];
+  } else if (userAgent.indexOf("Safari") > -1) {
+      browserName = "Safari";
+      fullVersion = userAgent.split("Version/")[1].split(" ")[0];
+  } else if (userAgent.indexOf("MSIE") > -1 || userAgent.indexOf("Trident") > -1) {
+      browserName = "Internet Explorer";
+      fullVersion = userAgent.split("MSIE ")[1].split(";")[0];
+  } else {
+      browserName = "Desconocido";
+      fullVersion = "Desconocido";
+  }
+
+  document.getElementById("browser-info").textContent = `Navegador: ${browserName}, Versión: ${fullVersion}`;
 }
